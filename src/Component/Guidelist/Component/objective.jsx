c1d1d900becf6b27b000d75b9cc4ee6c2554f090
naphatcmkl <85152768+naphatcmkl@users.noreply.github.com>
2022-10-22 16:44:59 +0700

    add little more guidelist

--- src/Component/Guidelist/Component/objective.jsx
+++ src/Component/Guidelist/Component/objective.jsx
@@ -10,10 +10,48 @@ import DraftsIcon from '@mui/icons-material/Drafts';
 import SendIcon from '@mui/icons-material/Send';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
+import Typography from '@mui/material/Typography';
 import StarBorder from '@mui/icons-material/StarBorder';
 
 export default function Objective() {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
+  const [isopen, setExpand] = React.useState([]);
+
+  const guide = [
+    {
+      id:0,
+      title:"Program Educational Objectives",
+      content:"jdakdasdnakdnaskjdnajkdnajkda",
+      opening:false
+    },
+    {
+      id:1,
+      title:"Student Outcomes",
+      content:"jdakdasdnakdnaskjdnajkdnajkda",
+      opening:false
+    },
+    {
+      id:2,
+      title:"Possible Career",
+      content:"jdakdasdnakdnaskjdnajkdnajkda",
+      opening:false
+    },
+  ]
+
+  let i = 0;
+  while (i < length(guide)){
+    let t = isopen;
+    t.push(false);
+    setExpand(t);
+    i += 1;
+
+  }
+
+  const handleExpand =(pos)=>{
+    let t = isopen;
+    t[pos] = !t[pos];
+    setExpand(t)
+  }
 
   const handleClick = () => {
     setOpen(!open);
@@ -26,18 +64,37 @@ export default function Objective() {
         <ListItemIcon>
           <InboxIcon />
         </ListItemIcon>
-        <ListItemText primary="Inbox" />
+        <ListItemText primary="Program Educational Objectives" />
         {open ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
       <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
+        {/* <List component="div" disablePadding>
           <ListItemButton sx={{ pl: 4 }}>
             <ListItemIcon>
               <StarBorder />
             </ListItemIcon>
             <ListItemText primary="Starred" />
           </ListItemButton>
-        </List>
+        </List> */}
+        <Typography component="div" disablePadding sx={{ pl: 4 }}>
+          dsmdkadklamdkasmdkadmalda
+        </Typography>
+
+      </Collapse>
+
+
+      <ListItemButton onClick={handleClick}>
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary="Student Outcomes" />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <Typography component="div" disablePadding sx={{ pl: 4 }}>
+          dsmdkadklamdkasmdkadmalda
+        </Typography>
+
       </Collapse>
         
     </List>
--- src/Component/Guidelist/index.jsx
+++ src/Component/Guidelist/index.jsx
@@ -1,11 +1,14 @@
 import Objective from "./Component/objective"
+import Typography from '@mui/material/Typography';
 
 export default function Guidelist(){
     return(
-        
+        <div>
+        <Typography>Objectives & Outcomes</Typography>
         <Objective>
 
         </Objective>
+        </div>
        
     )
 }
